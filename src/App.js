5edd3027c797cb60d745f9c24b2e016c12dab920
unknown <fangy2543@hotmail.com>
2020-11-01 01:37:27 +0700

    Clean code

--- src/App.js
+++ src/App.js
@@ -18,14 +18,12 @@ function App() {
   const getRecipes = async () => {
     const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
     const data = await response.json()
-    console.log(data.hits)
     setRecipes(data.hits)
 
   }
 
   const updateSearch = e => {
     setSearch(e.target.value)
-    console.log(search)
   }
 
   const getSearch = e => {
